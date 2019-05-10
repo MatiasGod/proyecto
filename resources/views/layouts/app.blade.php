bc534429f4c1585010099dd69e95c28f69ce70de
Matias Godoy <mgodled@gmail.com>
2019-05-10 17:12:05 +0100

    - Footer y fondo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,16 @@
     @include('include.head')
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-</head>
-    @if($view_name=='auth.login' || $view_name=='auth.register' || $view_name=='auth.passwords.email' )
+</head >
     <body id="principal">
+    @if($view_name=='auth.login' || $view_name=='auth.register' || $view_name=='auth.passwords.email' )
         
         <div class="container-fluid mt-5" id="cuerpo">
             @yield('content')
         </div>
 
     @else
-    <body id="secondary">
+    <!-- <body id="secondary"> -->
         @include('include.header')
         
         <div class="container mt-5 mb-5" id="cuerpo" id="app">
